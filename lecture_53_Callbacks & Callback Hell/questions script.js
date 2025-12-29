8a3e760149c4cbefbcc24611761ab0b5dfdad5d5
Prathamesh-coder-01 <prathameshdabhade71@gmail.com>
2025-12-29 22:40:39 +0530

    Callbacks & Callback Hell Functions

---
+++ lecture_53_Callbacks & Callback Hell/questions script.js
@@ -0,0 +1,60 @@
+// let intervalId;
+// let counter =0;
+// intervalId=setInterval(()=>{
+//     counter++;
+//     if (counter>=2){
+//         clearInterval(intervalId);
+//     }
+//     console.log(`tick ${counter}`);
+// },50);
+// console.log ("Internal set up .");
+
+
+/**
+  op -->
+
+  Internal set up.tick 1 tick 2
+
+  */
+
+// console.log("Start");
+// setTimeout(() => console.log("first"), 50);
+// setTimeout(() => console.log("second"), 0);
+// setTimeout(() => console.log("third"), 20);
+// console.log("End");
+
+
+
+// let countdoenValue = 3;
+// const countdoenInterval = setInterval(() => {
+//     if (countdoenValue > 0) {
+//         console.log(countdoenValue);
+//         countdoenValue--;
+//     } else {
+//         console.log("Blast off!");
+//         clearInterval(countdoenInterval);
+//     }
+
+// }, 1000);
+
+
+// const image = ["image 1", "image 2", "image 3"];
+// let currentIndex = 0;
+// function displayNextImage() {
+//   console.log("Display :", image[currentIndex]);
+//   currentIndex = (currentIndex + 1) % image.length;
+// }
+
+
+// console.log("Task A");
+// setTimeout(() => {
+//   let i = 0;
+//   while (i < 500000) {
+//     i++;
+
+//   }
+//   console.log("Task B Completed");
+
+
+// }, 0);
+// console.log("Task C");
